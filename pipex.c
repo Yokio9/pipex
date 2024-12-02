9900aca434dde8fe5ecd785bab8a4c313da9be47
Yokio9 <dmatayi@gmail.com>
2024-12-02 17:26:07 +0100

    works with argc argv

--- pipex.c
+++ pipex.c
@@ -12,15 +12,22 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int main()
+int main(int argc, char *argv[])
 {
 	int	i;
 	char	full_path[256] = {0};
 
 	i = 0;
-	char *args[] = {"ls", "-l", NULL};
+	char **args = malloc((argc - 1) * sizeof(char *));
+	while (argv[i + 1])
+	{
+		args[i] = strdup(argv[i + 1]);
+		i++;
+	}
 	const char *path_list[] = {"/usr/local/bin/", "/usr/bin/", "/bin/", "/sbin/", "/usr/sbin/", NULL};
+	i = 0;
 	while(path_list[i])
 	{
 		memset(full_path, 0, 256);
@@ -31,3 +38,7 @@ int main()
 	printf("couldnt find command\n");
 	return 0;
 }
+	//char *args[] = {"ls", "-l", NULL};
+
+	//settings set target.run-args ls
+	//frame variable
